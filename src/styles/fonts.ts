65c8bcc93c16dc6f4d3602e01114591b6c361b7d
Chiagoziem Sunday <gozmansunday@gmail.com>
2025-03-08 00:15:59 +0100

    chore: setup tailwindcss and fonts

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -4,9 +4,12 @@ import { Geist, Geist_Mono } from "next/font/google";
 export const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
+  display: "swap",
+  style: ["normal"],
 });
-
 export const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+  display: "swap",
+  style: ["normal"],
 });
